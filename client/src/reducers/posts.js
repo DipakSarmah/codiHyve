c4b100e3094407a9db4d1758be5e653820d84112
dipak sarmah <sarmahd60@gmail.com>
2023-03-29 02:48:57 +0530

    removed bugs

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,6 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
-export default function (posts = [], action) {
+export default posts = (posts = [], action)=> {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload;
